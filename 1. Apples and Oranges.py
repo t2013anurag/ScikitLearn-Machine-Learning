3bad3731b51c602ad771be9a82b679a2370fcf77
t2013anurag <tiwari.anurag126@gmail.com>
2016-04-08 16:09:13 +0530

    Program 1

---
+++ 1. Apples and Oranges.py
@@ -0,0 +1,11 @@
+# Using Decision tree
+from sklearn import tree
+
+# features = [[140, "smooth"], [130, "smooth"], [150, "bumpy"], [170, "bumpy"]]
+# labels = ["apple", "apple", "orange", "orange"]
+
+features = [[140, 1], [130, 1], [150, 0], [170, 0]]
+labels = [0, 0, 1, 1]
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features, labels)
+print clf.predict([[150, 0]])
