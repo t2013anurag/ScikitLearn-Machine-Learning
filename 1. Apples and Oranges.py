ec71c940d2c31e4af80e846aa66b9863e590d097
t2013anurag <tiwari.anurag126@gmail.com>
2016-04-10 00:47:36 +0530

    Random Dataset

--- 1. Apples and Oranges.py
+++ 1. Apples and Oranges.py
@@ -7,8 +7,11 @@ from sklearn import tree
 # smooth : 1, bumpy : 0
 # apple : 0, orange : 1
 
-features = [[140, 1], [130, 1], [150, 0], [170, 0]]
-labels = [0, 0, 1, 1]
+#features = [[140, 1], [130, 1], [150, 0], [170, 0]]
+# Checking with more features
+# labels = [0, 0, 1, 1]
+features = [[140, 1], [130, 1], [150, 0], [170, 0],[180, 1], [120, 1], [142, 0], [150, 0]]
+labels = [0, 1, 1, 1, 1, 1, 1, 0]
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(features, labels)
-print clf.predict([[150, 0]])
+print clf.predict([[145, 0]])
