65c741999b8fe8181f61d19e87415d5aa08ac899
Anurag Tiwari <tiwari.anurag126@gmail.com>
2016-06-04 21:28:10 +0530

    Pipeline

---
+++ pipeline.py
@@ -0,0 +1,24 @@
+from sklearn import datasets
+iris = datasets.load_iris()
+
+X = iris.data
+y = iris.target
+
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = .5)
+
+# decision tree
+# from sklearn import tree
+# my_classifier = tree.DecisionTreeClassifier()
+
+# k-neighbour classifier
+from sklearn.neighbors import KNeighborsClassifier
+my_classifier = KNeighborsClassifier()
+
+my_classifier.fit(X_train, y_train)
+predictions = my_classifier.predict(X_test)
+print predictions
+
+
+from sklearn.metrics import accuracy_score
+print accuracy_score(y_test, predictions)
