b6c34f7a36f1f6256b691965b02f5ed8f0140922
Anurag Tiwari <tiwari.anurag126@gmail.com>
2016-06-09 16:45:40 +0530

    Minor

--- Practice/NearestNeighbors.py
+++ Practice/NearestNeighbors.py
@@ -1,10 +1,15 @@
+import timeit
 from sklearn.neighbors import NearestNeighbors
+
 import numpy as np
+start = timeit.default_timer()
 # X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
 X = np.array([[1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6]]) #gives the euclidean distance
 nbrs = NearestNeighbors(n_neighbors = 3, algorithm = 'ball_tree').fit(X)
-#nbrs = NearestNeighbors(n_neighbors = 3, algorithm = 'kd_tree').fit(X)
+# nbrs = NearestNeighbors(n_neighbors = 3, algorithm = 'kd_tree').fit(X)
 # n_neighbors  = 3 will give 3 nearest neighbors.
 distances, indices = nbrs.kneighbors(X)
 print indices
 print distances
+stop = timeit.default_timer()
+print stop - start
