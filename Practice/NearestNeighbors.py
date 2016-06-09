e24067e11c5ca7ecceeca6ecc245636b86f2b621
Anurag Tiwari <tiwari.anurag126@gmail.com>
2016-06-09 16:29:29 +0530

    KD Tree

--- Practice/NearestNeighbors.py
+++ Practice/NearestNeighbors.py
@@ -3,6 +3,8 @@ import numpy as np
 # X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
 X = np.array([[1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6]]) #gives the euclidean distance
 nbrs = NearestNeighbors(n_neighbors = 3, algorithm = 'ball_tree').fit(X)
+#nbrs = NearestNeighbors(n_neighbors = 3, algorithm = 'kd_tree').fit(X)
+# n_neighbors  = 3 will give 3 nearest neighbors.
 distances, indices = nbrs.kneighbors(X)
 print indices
 print distances
