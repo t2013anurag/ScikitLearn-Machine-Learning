7022728d012ad0a8a3ca5d47ffb3d56ef85a91a8
Anurag Tiwari <tiwari.anurag126@gmail.com>
2016-07-04 12:13:14 +0530

    Creating own knn

--- create_pipeline.py
+++ create_pipeline.py
@@ -1,3 +1,8 @@
+from scipy.spatial import distance
+
+def euclidean_distance(a, b):
+	return distance.euclidean(a, b)
+
 import random
 
 class ScrappyKNN():
@@ -8,16 +13,30 @@ class ScrappyKNN():
 	def predict(self, X_test):
 		predictions = []
 		for row in X_test:
-			label = random.choice(self.y_train)
+			label = self.closest(row)
 			predictions.append(label)
 		return predictions
 
+	def closest(self, row):
+		# print row
+		# print self.X_train[0]
+		# print y_train
+		best_dist = euclidean_distance(row, self.X_train[0])
+		best_index = 0
+		for i in range(1, len(self.X_train)):
+			dist = euclidean_distance(row, self.X_train[i])
+			if dist < best_dist:
+				best_index = i
+			# print best_index
+		# print self.y_train[best_index]
+		return self.y_train[best_index]
 
 from sklearn import datasets
 iris = datasets.load_iris()
 
 X = iris.data
 y = iris.target
+print y
 
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = .5)
